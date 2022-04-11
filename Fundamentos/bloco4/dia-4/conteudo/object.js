01bbedd08d78310fcd553083cb5823d627a1cdb3
daviazevdo <azvd.davi@gmail.com>
2022-04-11 20:06:32 -0300

    adiciona os exercicios de objetos

---
+++ Fundamentos/bloco4/dia-4/conteudo/object.js
@@ -0,0 +1,22 @@
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white'
+//   };
+
+
+// console.log(car.type, car.model)
+
+
+let diasDaSemana = {
+    1: 'domingo',
+    2: 'segunda',
+    3: 'terça',
+    4: 'quarta',
+    5: 'quinta',
+    6: 'sexta',
+    7: 'sábado',
+    };
+    
+// console.log(diasDaSemana.1);  // SyntaxError: Unexpected number
+console.log(diasDaSemana['1']); // domingo
