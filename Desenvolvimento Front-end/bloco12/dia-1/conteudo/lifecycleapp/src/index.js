44ce1ddc9e6e19d778b0aee17d303758741fddac
daviazevdo <azvd.davi@gmail.com>
2022-06-30 12:46:00 -0300

    adiciona o comp RandomUser para o outro exercicio

--- Desenvolvimento Front-end/bloco12/dia-1/conteudo/lifecycleapp/src/index.js
+++ Desenvolvimento Front-end/bloco12/dia-1/conteudo/lifecycleapp/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RandomUser from './components/RandomUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RandomUser />
   </React.StrictMode>
 );
 
