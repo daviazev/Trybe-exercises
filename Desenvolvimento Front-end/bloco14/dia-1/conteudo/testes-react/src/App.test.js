e9627201887a51690e0962da444382dd8bcc03c3
daviazevdo <azvd.davi@gmail.com>
2022-07-06 23:17:05 -0300

    adiciona outra parte do conteudo do dia 14.1

--- Desenvolvimento Front-end/bloco14/dia-1/conteudo/testes-react/src/App.test.js
+++ Desenvolvimento Front-end/bloco14/dia-1/conteudo/testes-react/src/App.test.js
@@ -1,12 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 it('verifica se tem um input na tela', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // const mytest = screen.getByText(/teste/i);
-  // expect(mytest).toBeInTheDocument();
-  // expect(linkElement).toBeInTheDocument();
 
   const inputName = screen.getByLabelText('Name');
 
@@ -18,7 +16,7 @@ it('Verifica se há dois botões na tela', () => {
   render(<App />);
 
   const buttons = screen.getAllByRole('button');
-  expect(buttons).toHaveLength(2);
+  expect(buttons).toHaveLength(3);
 });
 
 it('Verifica se existe o botão "Virus" na tela', () => {
@@ -39,4 +37,53 @@ it('Verifica se existe o botão "Clique" na tela', () => {
   expect(cliqueButton).toBeInTheDocument();
   expect(cliqueButton).toHaveTextContent('Clique'); // do texto dentro do componente
   expect(cliqueButton).toHaveValue(''); // da propriedade value
+});
+
+it('Verifica se existe o input de Email na tela', () => {
+  render(<App />);
+
+  const labelEmail = screen.getByLabelText('Email');
+
+  expect(labelEmail).toBeInTheDocument();
+  // expect(labelEmail).toHaveTextContent('Email'); // do texto dentro do componente
+  expect(labelEmail).toHaveValue(''); // da propriedade value
+  expect(labelEmail).toHaveProperty('type', 'email'); // trata das propriedades
+  expect(labelEmail).toHaveProperty('id', 'id-email'); // toHaveProperty(propriedade, valor)
+});
+
+it('Verifica um botão', () => {
+  render(<App />);
+
+  const button = screen.getByTestId('btn-send');
+
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveProperty('type', 'button');
+  expect(button).toHaveValue('Enviar');
+});
+
+it('Verificando se o botão e o campo email estão funcionando.', () => {
+  render(<App />);
+
+  const EMAIL_USER = 'simplesmenteoemaildoentretenimento@test.sem.seila';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('btn-send');
+  const inputEmail = screen.getByLabelText('Email');
+
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+});
+
+it('Verifica um input pelo placeholder', () => {
+  render(<App />);
+
+  const inputByPlaceholder = screen.getByPlaceholderText('escreva seu nome :)');
+
+  expect(inputByPlaceholder).toBeInTheDocument();
 });
