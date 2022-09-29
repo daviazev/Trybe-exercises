73af8475a3af89279483187f7fee655f3dce718e
daviazevdo <azvd.davi@gmail.com>
2022-09-28 21:53:02 -0300

    conteudo do dia 22.2

---
+++ Desenvolvimento Back-end/bloco22/dia-2/conteudo/soccer-team-manager/src/app.js
@@ -0,0 +1,74 @@
+// src/app.js
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SAO',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (_req, res) =>
+  res.status(200).json({ message: 'Olá Mundo! xablau' }));
+
+app.get('/teams', (_req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+// Aqui, estamos trabalhando com método PUT, 
+// o qual é utilizado quando queremos alterar um
+//  recurso. Ele também recebe dados pelo corpo da requisição.
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+// Este trecho de código fui eu que fiz, e SOZINHOOOOOOOO!
+
+app.get('/teams/:id', (req, res) => {
+  const { id } = req.params;
+
+  const findTeamById = teams.find((team) => team.id === parseFloat(id));
+
+  res.status(200).json({ result: findTeamById });
+});
+
+// O TRECHO ACIMA FOI EU QUEM FIZ AAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+  res.status(200).end();
+});
+
+module.exports = app;
