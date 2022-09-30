d46d716ab460898ee1a8e52ee7b6d7bb6b2493cd
daviazevdo <azvd.davi@gmail.com>
2022-09-30 17:38:29 -0300

    construção das rotas para cada cenário de teste

--- Desenvolvimento Back-end/bloco22/dia-3/conteudo/cacao-trybe/src/app.js
+++ Desenvolvimento Back-end/bloco22/dia-3/conteudo/cacao-trybe/src/app.js
@@ -3,9 +3,25 @@ const express = require('express');
 const app = express();
 const cacaoTrybe = require('./cacaoTrybe');
 
+const OK = 200;
+
 app.get('/chocolates', async (_req, res) => {
   const chocolates = await cacaoTrybe.getAllChocolates();
-  res.status(200).json({ chocolates });
+  res.status(OK).json({ chocolates });
+});
+
+app.get('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const response = await getChocolateById(Number(id));
+  res.status(OK).json({ response });
+});
+
+app.get('/chocolates/brand/:brandId', async (req, res) => {
+  const { brandId } = req.params;
+
+  const response = await getBrandById(Number(brandId));
+  res.status(OK).json({ response });
 });
 
 module.exports = app;
