65026fda5c402354c025f1e72898bc8cd76dc6f6
daviazevdo <azvd.davi@gmail.com>
2022-11-01 14:29:16 -0300

    express.static - nao funcionando

--- Desenvolvimento Back-end/bloco22/dia-4/conteudo/times-de-futebol/src/app.js
+++ Desenvolvimento Back-end/bloco22/dia-4/conteudo/times-de-futebol/src/app.js
@@ -104,10 +104,11 @@ const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.static('/images'));
 app.use(express.json());
 // monta o router na rota /teams (1)
 app.use('/teams', teamsRouter);
+
+app.use(express.static('../images')); // NÃO FUNCIONOU :(
 
 app.use((err, _req, _res, next) => {
   console.error(err.stack);
