b427322ca7586ef0ad12633fb47f2517ff6caf51
daviazevdo <azvd.davi@gmail.com>
2022-11-01 13:55:24 -0300

    adiciona experimento usando express.json

---
+++ Desenvolvimento Back-end/bloco22/dia-4/conteudo/times-de-futebol/src/experimento-json.js
@@ -0,0 +1,19 @@
+// src/experimento-json.js
+
+// PARA VER ESTE EXEMPLO RODANDO, RODE O SEGUINTE COMANDO: node src/experimento-json.js
+
+const express = require('express');
+const app = express();
+
+app.post('/fail', (req, res) => {
+  res.status(200).json({ greeting: `Hello, ${req.body.nome}!` });
+});
+
+app.use(express.json());
+
+app.post('/hello', (req, res) => {
+  // req.body agora está disponível
+  res.status(200).json({ greeting: `Hello, ${req.body.nome}!` });
+});
+
+app.listen(3000, () => { console.log('Ouvindo na porta 3000'); });
