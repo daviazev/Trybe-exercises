c7ed891299a22059a358d21f298beef08225e32e
daviazevdo <azvd.davi@gmail.com>
2022-12-22 20:28:04 -0300

    add conteudo à parte de model com mysql

--- Desenvolvimento Back-end/bloco26/dia-2/model-com-mysql/models/Book.ts
+++ Desenvolvimento Back-end/bloco26/dia-2/model-com-mysql/models/Book.ts
@@ -1,7 +1,7 @@
 // ./models/Book.ts
 
 import connection from './connection';
-import { Pool } from 'mysql2/promise';
+import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 
 export interface Book {
   id?: number;
@@ -19,8 +19,21 @@ export default class BookModel {
   }
 
   public async getAll(): Promise<Book[]> {
-    const result = await this.connection.execute('SELECT * FROM books');
+    const result = await this.connection.execute<(Book & RowDataPacket)[]>(
+      'SELECT * FROM books'
+    );
     const [rows] = result;
     return rows as Book[];
+  }
+
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
+      [title, price, author, isbn]
+    );
+
+    return { id: insertId, ...book };
   }
 }
