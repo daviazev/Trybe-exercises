ae10f1a3b02ff10b2825f14a325d72be4352c16c
Davi Azevedo <azvd.davi@gmail.com>
2023-03-23 12:47:25 -0300

    conteúdo de manipulação de arquivos json e csv

--- computer-science/sessao-1/dia-2-entrada-e-saida-de-dados/conteudo/manipulando_arquivos_json.py
+++ computer-science/sessao-1/dia-2-entrada-e-saida-de-dados/conteudo/manipulando_arquivos_json.py
@@ -5,3 +5,26 @@
 # Alguns exemplos de utilização incluem comunicação back-end e front-end, e comunicação com sistemas externos (gateways de pagamento, por exemplo) ou internos (como um sistema de autenticação).
 
 # A linguagem Python já inclui um módulo para manipulação desse tipo de arquivo e seu nome é json.
+
+
+# Seus principais métodos para manipulação são: load, loads, dump, dumps.
+
+# 🐭 Para demonstrar como é feita a manipulação de arquivos JSON, vamos utilizar o arquivo pokemons.json que é uma lista de pokemons com suas informações chave.
+# link: https://lms-assets.betrybe.com/lms/pokemons.json
+
+# 💡 Para fazer o exemplo, coloque o arquivo pokemons.json no mesmo diretório em que estará o seu script. (arquivo pokemons.json)
+
+import json
+
+with open("pokemons.json") as file:
+    # content = file.read()
+    pokemons = json.load(file)['results']
+
+grass_type_pokemons = [
+    pokemon for pokemon in pokemons if "Grass" in pokemon["type"]
+]
+
+with open("grass_pokemons.json", "w") as file:
+    json.dump(grass_type_pokemons, file)
+
+print(pokemons[0])
